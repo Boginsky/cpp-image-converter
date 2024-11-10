234c7b19a320b835b1277f6c7d3f7955d70cbce2
Kirill Boginsky <kirill.boginsky.workspace@gmail.com>
2024-11-10 15:20:21 +0300

    Final converter version

---
+++ ImgLib/bmp_image.cpp
@@ -0,0 +1,104 @@
+#include "bmp_image.h"
+#include "pack_defines.h"
+
+#include <array>
+#include <fstream>
+#include <string_view>
+#include <iostream>
+
+using namespace std;
+
+namespace img_lib {
+    
+static int GetBMPStride(int w) {
+    return 4 * ((w * 3 + 3) / 4);
+}    
+    
+PACKED_STRUCT_BEGIN BitmapFileHeader {
+    BitmapFileHeader() = default;
+    BitmapFileHeader(int width, int height) {
+        sizeOfBitmapFile = height * GetBMPStride(width) + 54;
+    }
+    
+    char bitmapSignatureBytes[2] = {'B', 'M'};
+    unsigned int sizeOfBitmapFile;
+    uint32_t reservedBytes = 0;
+    unsigned int pixelDataOffset = 54;
+}
+PACKED_STRUCT_END
+
+PACKED_STRUCT_BEGIN BitmapInfoHeader {
+    BitmapInfoHeader() = default;
+    BitmapInfoHeader(int width, int height) : width_(width), height_(height) {
+        rawBitmapDataSize = height * GetBMPStride(width);
+    }
+
+    unsigned int sizeOfThisHeader = 40;
+    int width_;
+    int height_;
+    uint16_t numberOfColorPlanes = 1;
+    uint16_t colorDepth = 24;
+    uint32_t compressionMethod = 0;
+    uint32_t rawBitmapDataSize;
+    int horizontalResolution = 11811;
+    int verticalResolution = 11811;
+    int colorTableEntries = 0;
+    int importantColors = 0x1000000;
+}
+PACKED_STRUCT_END
+
+bool SaveBMP(const Path& file, const Image& image) {
+    BitmapFileHeader file_header_(image.GetWidth(), image.GetHeight());
+    BitmapInfoHeader info_header_(image.GetWidth(), image.GetHeight());
+    
+    ofstream out(file, ios::binary);
+    out.write((char*) &file_header_, 14);
+    out.write((char*) &info_header_, 40);
+
+    const int w = image.GetWidth();
+    const int h = image.GetHeight();
+    const int padding = GetBMPStride(w);
+    std::vector<char> buff(padding);
+
+    for (int y = h - 1; y >= 0; --y) {
+        const Color* line = image.GetLine(y);
+        for (int x = 0; x < w; ++x) {
+            buff[x * 3 + 0] = static_cast<char>(line[x].b);
+            buff[x * 3 + 1] = static_cast<char>(line[x].g);
+            buff[x * 3 + 2] = static_cast<char>(line[x].r);
+        }
+        out.write(buff.data(), padding);
+    }
+
+    return out.good();
+}
+
+Image LoadBMP(const Path& file) {
+    BitmapFileHeader file_header_;
+    BitmapInfoHeader info_header_;
+    ifstream ifs(file, ios::binary);
+
+    ifs.read((char *) &file_header_, 14);
+    ifs.read((char *) &info_header_, 40);
+
+    int w, h, padding;
+    w = info_header_.width_;
+    h = info_header_.height_;
+    padding = GetBMPStride(w);
+    Image result(w, h, Color::Black());
+
+    std::vector<char> buff(padding);
+    for (int y = h - 1; y >= 0; --y) {
+        Color* line = result.GetLine(y);
+        ifs.read(buff.data(), padding);
+
+        for (int x = 0; x < w; ++x) {
+            line[x].b = static_cast<byte>(buff[x * 3 + 0]);
+            line[x].g = static_cast<byte>(buff[x * 3 + 1]);
+            line[x].r = static_cast<byte>(buff[x * 3 + 2]);
+        }
+    }
+    return result;
+}
+
+}
